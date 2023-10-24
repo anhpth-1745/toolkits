efd06650e73af7816111ab71af7ce1df617749b5
anhpth <pham.thi.hong.anh@sun-asterisk.com>
2023-10-24 14:10:36 +0700

    use_pandasgui

--- vizualize_data/visual_w_1line.py
+++ vizualize_data/visual_w_1line.py
@@ -1,17 +1,25 @@
 import pandas as pd 
 import sweetviz as sv
 import IPython
+from pandasgui import show import pandas as pd
 
 
-#read data
+# use sweetviz
+## read data
 data = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
 
-# EDA data
+## EDA data
 my_report = sv.analyze(data)
 my_report.show_html()
 
-# show result
+## show result
 IPython.display.HTML('SWEETVIZ_REPORT.html')
 
 
+# use pandasGui
 
+## load data
+train = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv") 
+
+## show visualize
+gui = show(train)
